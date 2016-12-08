63a43da2d9db1e8f3add55011cedbce2d358a9cc
Leanne Lee <leannelee812@berkeley.edu>
2016-12-07 17:06:27 -0800

    start to write final report and import some images

--- travel.R
+++ travel.R
@@ -1,12 +1,19 @@
 library(ggplot2)
 library(zoo)
 library(forecast)
+library(lubridate)
+library(plyr)
 
 canada <- read.csv("./canada.csv", header = TRUE, sep = "")
 canada$Date <- as.Date(canada$Date)
-gg <- ggplot(data=canada ,aes(y=Value,x=Date)) + geom_line( colour = "blue") +
+canada_ymd <- mutate(canada, date = ymd(canada$Date), 
+       day = day(date), month = month(date), year = year(date))
+
+png("images/ts.png")
+gg <- ggplot(data=canada_ymd ,aes(y=Value,x=Date)) + geom_line( colour = canada_ymd$month) +
   ggtitle("Monthly Air travel to Canada")
-gg + geom_vline(xintercept=as.numeric(canada$Data),linetype=4)
+gg + geom_vline(xintercept=as.numeric(canada_ymd$Data),linetype=4) 
+dev.off()
 
 ##Exploratory Data Analysis
 
@@ -15,40 +22,47 @@ summary(canada$Value)
 sd(canada$Value)
 
 #STL
-travel<-ts(canada$Value,start=c(1995,1),end=c(2016,9),frequency=12)
+travel<-ts(canada$Value,start=c(1996,1),end=c(2016,9),frequency=12)
 stl_travel<-stl(travel,s.window = "periodic")
 plot(stl_travel, main="Seasonal Decomposition of Time Series by Loess for monthly Canada Travels")
 #stl shows that there is a seasonal trend
 #The bar on the seasonal panel is only a bit larger than the data panel, meaning the
 #seasoning signal is large relative to the variation in the data
 # The trend panel has larger box than the data and seasonal panels, meaning 
-# the variation attrivuted to the trend is smaller than seasonal component, which
-# doesn't exists a trend 
+# the variation attrivuted to the trend is smaller than seasonal component, 
+# which the trend is not dominating. But there's an upward trend at the end
 
 #In the data panel, there is no obvious sign of non-stationary, so we don't need to 
 # take differencing for now. 
 
+#plot the residual aginst time 
+residuals(travel)
+
 #Plotting
+png("images/monthly_plot.png")
 m <-c('J','F','M','A','M','J','J','A','S','O','N','D')
-plot(canada$Value,ylab='Number of arrivals',xlab='Time Index', type = 'l', 
+plot(canada$Value, ylab='Number of arrivals',xlab='Time Index', type = 'l', 
      main="Monthly Air arrivals to Canada")
 points(canada$Value,pch=m)
+dev.off()
+
+
 #The peak of the traveling season is July and August
-#Fit log series
-plot(log(canada$Value),ylab='Number of Arrivals',xlab='Time', type = 'l',
-     main="Logarithm of Monthly air travel to Canada")
-points(log(canada$Value),pch=m)
+#Because the graph doesn't seem like it get effect when the variation increases with the level of the series,
+#so there is no need of taking logs 
 
 #ACF for MA
 acf(canada$Value, main = "ACF for air travel to Canada")
-acf(log(canada$Value),lag.max=36, main = "ACF for log Air Travel to Canada") 
+acf(log(canada$Value),lag.max=60, main = "ACF for log Air Travel to Canada") 
 #the acf appear to be seasonality 
-acf(diff(log(canada$Value),12),lag.max=36, ci.type='ma',
+acf(diff(log(canada$Value),12),lag.max=60, ci.type='ma',
       main="ACF of First Difference of logarithm of Air Travel to Canada")
-acf(diff(diff(log(canada$Value)),12),lag.max=36,ci.type='ma',
+acf(diff(diff(log(canada$Value)),12),lag.max=60,ci.type='ma',
     main="ACF of Twice Difference of logarithm of Air Travel to Canada") 
 
 #PACF for AR 
+pacf(canada$Value, main = "PACF for air trave to Canada")
+pacf(log(canada$Value), lag.max = 60, main = "PACF for log Air Travel to Canada")
 pacf(diff(log(canada$Value),12),lag.max=36,
      main="PACF of First Difference of logarithm of Air Travel to Canada") 
 pacf(diff(diff(log(canada$Value)),12),lag.max=36,
@@ -62,6 +76,7 @@ eacf(log(canada$Value))
 
 ##ARIMA Models
 
+#put the canada data into month matrix 
 data <- ts(canada$Value, freq = 12)
 #run auto arima
 auto <- auto.arima(log(data))
@@ -71,44 +86,35 @@ tsdiag(auto)
 acf(residuals(auto), lag.max = 36)
 #Auto ARIMA shows that there is no differencing and it's equivalent to ARMA(3,1)
 
-#Without seasonal effect (should I ignore the seasonal effect ???)
-f1 <- arima(log(data), order = c(1,1,1))
-tsdisplay(residuals(f1))
-tsdiag(f1)
-acf(residuals(f1), lag.max = 36)
-
-f2 <- arima(log(data), order = c(2,1,2))
-tsdisplay(residuals(f2))
-tsdiag(f2)
-acf(residuals(f2), lag.max = 36)
-
-f3 <- arima(log(data), order = c(2,1,0))
-tsdisplay(residuals(f3))
-tsdiag(f3)
-acf(residuals(f3), lag.max = 36)
-
 #With seasonal effect 
 #First model with seasonal effect 
 fit <- arima(log(data), order = c(2,0,2), seasonal=list(order = c(0,1,1), period = 12))
 tsdisplay(residuals(fit))
 tsdiag(fit)
-acf(residuals(fit2), lag.max = 36)
-pacf(residuals(fit2))
+acf(residuals(fit))
+pacf(residuals(fit))
 
 #Second model with seasonal effect 
-fit1 <-arima(log(data), order=c(0,1,1), seasonal=list(order = c(0,1,1), period = 12))
+fit1 <-arima(log(data), order=c(2,1,2), seasonal=list(order = c(0,1,1), period = 12))
 tsdisplay(residuals(fit1))
 tsdiag(fit1)
 plot(fit1)
-acf(residuals(fit1), lag.max = 36)
+acf(residuals(fit1))
+pacf(residuals(fit1))
 
 #Third model with seasonal effect
 fit2 <- arima(log(data), order=c(2,0,1), seasonal=list(order = c(0,1,1), period = 12))
 tsdisplay(residuals(fit2))
 tsdiag(fit2)
 plot(fit2)
-acf(residuals(fit2), lag.max = 36)
-McLeod.Li.test(fit2) #above 0.5 of p-values meaning didn't reject H0. 
+acf(residuals(fit2))
+#Fit2 has the smallest AIC 
+
+##check p and q with auto
+##plot fitted model residual against time and find out what happen to peaks
+
+McLeod.Li.test(fit2) 
+#above 0.5 of p-values meaning didn't reject H0. 
 #no arch effect so don't need arch-garch 
 
 #try sin and cosine  and compare the forecast 
@@ -117,9 +123,9 @@ resModel <- residuals(fit2)
 #Normal QQ Plot for residuals
 qqnorm(resModel)
 qqline(resModel)
+# There are a few outlier in the beginnings 
 hist(resModel,xlab='Standardized Residuals for the fitted model')
 #shapiro.test(resModel)
-
 
 # From Frank Davenport
 funggcast <- function(dn,fcast){ 
@@ -148,17 +154,11 @@ funggcast <- function(dn,fcast){
   
 }
 
-#canada_date <- window(canada$Date)
-#model_fit <- arima(canada_date, order=c(2,0,1), seasonal=list(order = c(0,1,1), period = 12))
-# how come this one doesn't have drift??
-
-best_model <- Arima(log(canada$Value), order = c(2,0,1))
-#why i can only forecast the auto.arima??????? 
-#plot(forecast(best_model))
+best_model <- Arima(log(data), order=c(2,0,1), seasonal=list(order = c(0,1,1), period = 12))
 plot(forecast(best_model, h = 12))
 abline(h=auto$coef["intercept"], lty=2) #draw a horizontal line in the mean
-model_forecast <- forecast(auto)
-model_df <- funggcast(auto, model_forecast)
+model_forecast <- forecast(best_model)
+model_df <- funggcast(best_model, model_forecast)
 #error!! 
 
 ggplot_forecast <- function(pd) {
@@ -174,22 +174,26 @@ ggplot_forecast <- function(pd) {
 }
 ggplot_forecast(model_df)
 
+
+canada_train <- window(canada, end = 2011.99)
+
 #Detech outlier
 detectAO(fit2) #Additive Outliers
 detectIO(fit2) #Innovative Outliers
 #The magnitude (lambda) of IO is bigger so IO has more effect than AO.
 #the same index 81 has an outlier but it doesn't detech the outliers at the end of series
-
+#IO tells more story 
 
 ## Spectral Analysis
 p <- periodogram(resModel, main = "Periodogram for the residuals of the model")
 p
+##plot = "y"??? 
 ?spec.pgram()
 spec(resModel,main="Periodogram", kernel = kernel("daniell", c(3,3)), taper = 0.05,
      ci.plot = T)
 #Can draw a flat line between the interval so there is no ambigitiy
-
+# Very stable 
 
 ## Arch Garch Model
 garch(data, order = c(1,1))
-
+#only one bump at the end so 1,1 is good 
